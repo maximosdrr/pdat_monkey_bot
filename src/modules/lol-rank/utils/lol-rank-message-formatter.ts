f7e6a389775c7e4df7b02ffb423efd73d600b20c
Hiran Júnior <hiran.junior@rankmyapp.com.br>
2022-05-05 19:38:28 -0300

    feat: white list

--- src/modules/lol-rank/utils/lol-rank-message-formatter.ts
+++ src/modules/lol-rank/utils/lol-rank-message-formatter.ts
@@ -2,17 +2,35 @@ import { AppConfig } from "../../../config/env";
 import { SummonerRankData } from "../interfaces/interfaces";
 
 export class LolRankMessageFormatter {
+  private sortRank(playersQueueData: SummonerRankData[]): SummonerRankData[] {
+    const sorted = playersQueueData.sort((a, b) => {
+      if (a.points > b.points) {
+        return -1;
+      }
+
+      if (b.points > a.points) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    return sorted;
+  }
+
   hacker(summoners: SummonerRankData[]) {
     const whiteList = AppConfig.whiteList.split(",");
     console.log(whiteList);
 
-    return summoners.map((summoner) => {
+    const summonersFormat = summoners.map((summoner) => {
       console.log(summoner.summonerName);
       if (whiteList.includes(summoner.summonerName)) {
         summoner.points += 50;
       }
       return summoner;
     });
+
+    return this.sortRank(summonersFormat);
   }
 
   formatMessage(summoners: SummonerRankData[]) {

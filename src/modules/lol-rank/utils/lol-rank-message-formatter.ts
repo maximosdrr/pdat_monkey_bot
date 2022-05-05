564bfaa83dcc20fda6132152fd4683bf3c36d379
Hiran Júnior <hiran.junior@rankmyapp.com.br>
2022-05-05 19:16:08 -0300

    feat: meme

--- src/modules/lol-rank/utils/lol-rank-message-formatter.ts
+++ src/modules/lol-rank/utils/lol-rank-message-formatter.ts
@@ -1,11 +1,22 @@
 import { SummonerRankData } from "../interfaces/interfaces";
 
 export class LolRankMessageFormatter {
+  hacker(summoners: SummonerRankData[]) {
+    for (const summoner of summoners) {
+      if (summoner.summonerName === "Incrivel Hoók") {
+        summoner.points += 50;
+      }
+    }
+
+    return summoners;
+  }
+
   formatMessage(summoners: SummonerRankData[]) {
+    const meta = this.hacker(summoners);
     let data = "";
     let i = 1;
 
-    for (const summoner of summoners) {
+    for (const summoner of meta) {
       data += `⭐ ${i} - ${summoner.summonerName} = ${summoner.points}\n`;
       i += 1;
     }
